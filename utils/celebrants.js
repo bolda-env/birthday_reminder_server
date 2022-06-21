c109db0951ef070182c34db36fcb09098f3b54c6
bolda-env <mannydev02@gmail.com>
2022-06-21 05:45:45 +0100

    modify celebrants.js

--- utils/celebrants.js
+++ utils/celebrants.js
@@ -1,32 +1,37 @@
 exports.data = [
   {
     id: 1,
-    avatar: 'img_9392',
+    avatar: 'img_9392.JPG',
     name: 'abel emmanuel',
     year: 22,
   },
   {
     id: 2,
+    avata: 'img_5d8d246878273.jpg',
     name: 'oguche isreal',
     year: 24,
   },
   {
     id: 3,
+    avatar: 'img_5d9c6a60c1a11.png',
     name: 'abu simon',
     year: 26,
   },
   {
     id: 4,
+    avatar: 'img_5d9c6bc3d92f3.png',
     name: 'thankgod michael',
     year: 34,
   },
   {
     id: 5,
+    avatar: 'img_5d9c705f1020c.png',
     name: 'john doe',
     year: 54,
   },
   {
     id: 6,
+    avatar: 'img_5ddc5e8c19d7f.png',
     name: 'lukeman yusuf',
     year: 75,
   },
